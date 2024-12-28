301994f56aaee5631aa4377baf3283c596f9a09a
Walaa VOILIDAS <walaavolidis@gmail.com>
2024-12-28 10:44:56 +0200

    push changes

--- src/app/hooks/use-deck.ts
+++ src/app/hooks/use-deck.ts
@@ -1,49 +1,8 @@
 import { useState } from 'react';
+import { Deck, generateDeck } from '../deck';
 export function useDeck() {
-  const generateDeck = () => {
-    const suits: string[] = ['♠', '♥', '♦', '♣'];
-    const values: string[] = [
-      'A',
-      '2',
-      '3',
-      '4',
-      '5',
-      '6',
-      '7',
-      '8',
-      '9',
-      '10',
-      'J',
-      'Q',
-      'K',
-    ];
-    const deck: { suit: string; value: string }[] = [];
-
-    suits.forEach((suit) => {
-      values.forEach((value) => {
-        deck.push({ suit, value });
-      });
-    });
-
-    for (let i = deck.length - 1; i > 0; i--) {
-      const randomIndex = Math.floor(Math.random() * (i + 1));
-      [deck[randomIndex], deck[i]] = [deck[i], deck[randomIndex]];
-    }
-
-    return deck;
-  };
-  const [deck, setDeck] = useState<{ suit: string; value: string }[]>(
-    generateDeck()
-  );
-  const [drawnCards, setDrawnCards] = useState<
-    { suit: string; value: string }[]
-  >([]);
-  const [deckCount, setDeckCount] = useState(1);
-
-  const addDeck = () => {
-    setDeckCount((prev) => prev + 1);
-    setDeck((prev) => [...prev, ...generateDeck()]);
-  };
+  const [deck, setDeck] = useState<Deck>(generateDeck());
+  const [drawnCards, setDrawnCards] = useState<Deck>([]);
 
   const refreshDeck = () => {
     setDeck(generateDeck());
@@ -62,9 +21,7 @@ export function useDeck() {
   return {
     generateDeck,
     deck,
-    deckCount,
     drawnCards,
-    addDeck,
     refreshDeck,
     drawCards,
   };
