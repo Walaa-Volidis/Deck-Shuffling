c27dda0121166c5a6a2a12c91339c3dc852141ff
Walaa VOILIDAS <walaavolidis@gmail.com>
2024-12-29 10:28:55 +0200

    Persist game state across refreshes using localstorage

--- src/app/hooks/use-deck.ts
+++ src/app/hooks/use-deck.ts
@@ -1,17 +1,30 @@
 import { useEffect, useState } from 'react';
 import { Deck, generateDeck } from '../deck';
 export function useDeck() {
-  const [deck, setDeck] = useState<Deck>(generateDeck());
-  const [drawnCards, setDrawnCards] = useState<Deck>([]);
+  const [deck, setDeck] = useState<Deck>(
+    localStorage.getItem('deck')
+      ? JSON.parse(localStorage.getItem('deck') as string)
+      : generateDeck()
+  );
+  const [drawnCards, setDrawnCards] = useState<Deck>(
+    localStorage.getItem('drawnCards')
+      ? JSON.parse(localStorage.getItem('drawnCards') as string)
+      : []
+  );
   const [suffledDeck, setSuffledDeck] = useState<Deck>([]);
 
   //auto draw cards after 5sec
   useEffect(() => {
+    localStorage.setItem('deck', JSON.stringify(deck));
     const interval = setInterval(() => {
       drawCards();
     }, 5000);
     return () => clearInterval(interval);
   }, [deck]);
+
+  useEffect(() => {
+    localStorage.setItem('drawnCards', JSON.stringify(drawnCards));
+  }, [drawnCards]);
 
   const refreshDeck = () => {
     //store the last 10 suffled cards
@@ -34,7 +47,6 @@ export function useDeck() {
   const addDeck = () => {
     setDeck([...deck, ...generateDeck()]);
   };
-
 
   return {
     generateDeck,
