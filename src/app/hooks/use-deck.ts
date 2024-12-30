989777bf2b2834918cac226faadf047907ad8aeb
Walaa VOILIDAS <walaavolidis@gmail.com>
2024-12-30 09:39:13 +0200

    use shuffleDeck in refreshDeck and get values from localstorage using hook instead of put as initial value in state

--- src/app/hooks/use-deck.ts
+++ src/app/hooks/use-deck.ts
@@ -1,19 +1,17 @@
 import { useEffect, useState } from 'react';
-import { Deck, generateDeck } from '../deck';
+import { Deck, generateDeck, shuffleDeck } from '../deck';
 export function useDeck() {
-  const [deck, setDeck] = useState<Deck>(
-    localStorage.getItem('deck')
-      ? JSON.parse(localStorage.getItem('deck') as string)
-      : generateDeck()
-  );
-  const [drawnCards, setDrawnCards] = useState<Deck>(
-    localStorage.getItem('drawnCards')
-      ? JSON.parse(localStorage.getItem('drawnCards') as string)
-      : []
-  );
-  const [suffledDeck, setSuffledDeck] = useState<Deck>([]);
+  const [deck, setDeck] = useState<Deck>(generateDeck());
+  const [drawnCards, setDrawnCards] = useState<Deck>([]);
+  const [shuffledDeck, setShuffledDeck] = useState<Deck>([]);
 
-  //auto draw cards after 5sec
+  useEffect(() => {
+    const drawn = JSON.parse(localStorage.getItem('drawnCards') as string);
+    setDrawnCards(drawn);
+    const cards = JSON.parse(localStorage.getItem('deck') as string);
+    setDeck(cards);
+  }, []);
+
   useEffect(() => {
     localStorage.setItem('deck', JSON.stringify(deck));
     const interval = setInterval(() => {
@@ -26,12 +24,12 @@ export function useDeck() {
     localStorage.setItem('drawnCards', JSON.stringify(drawnCards));
   }, [drawnCards]);
 
-  const refreshDeck = () => {
-    //store the last 10 shuffled cards
-    const newDeck = generateDeck();
-    setDeck(newDeck);
+  const refreshDeck = (deckCount: number = 1) => {
+    const newDeck = generateDeck(deckCount);
+    const newShuffledDeck = shuffleDeck(newDeck);
+    setDeck(newShuffledDeck);
     setDrawnCards([]);
-    setSuffledDeck(newDeck.slice(-10));
+    setShuffledDeck(newShuffledDeck.slice(-10));
   };
 
   const drawCards = () => {
@@ -43,7 +41,6 @@ export function useDeck() {
     setDrawnCards(drawn);
   };
 
-  // add deck
   const addDeck = () => {
     setDeck([...deck, ...generateDeck()]);
   };
@@ -55,5 +52,6 @@ export function useDeck() {
     refreshDeck,
     drawCards,
     addDeck,
+    shuffledDeck,
   };
 }
